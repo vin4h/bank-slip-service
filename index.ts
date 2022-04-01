e2ba2c55959038040454f3b28cb77b1b3459c9fa
Vinicius Freitas <viniciu.freitas@gmail.com>
2022-03-31 20:56:51 -0400

    alter name handler

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { barCodeIsNumberValidate } from './utils/validate'
 type barCode = number;
 export const bankSlipService = async (event: any) => {
-  if (event.http.method != 'GET') {
+  if (event.httpMethod != 'GET') {
     return {
       statusCode: 405,
       body: JSON.stringify({
@@ -10,13 +10,19 @@ export const bankSlipService = async (event: any) => {
     }
   }
   const barCode: barCode = event.pathParameters.barCode;
-
-  if(barCodeIsNumberValidate(barCode)) {
+  if(!barCodeIsNumberValidate(barCode)) {
     return {
       statusCode: 400,
       body: JSON.stringify({
         message: 'BarCode must be a number or contains 44 numbers',
       }),
     }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'BarCode is valid',
+    }),
   }
 };
