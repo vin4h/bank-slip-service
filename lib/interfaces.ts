90bf1cc7afc7ebe4fb2dedce653e34b4da4bbcb9
Vinicius Freitas <viniciu.freitas@gmail.com>
2022-04-02 08:38:04 -0400

    add interfaces

---
+++ lib/interfaces.ts
@@ -0,0 +1,11 @@
+export interface IFieldsToCalculate {
+  firstFieldCalculate: string;
+  secondFieldCalculate: string;
+  thirdFieldCalculate: string;
+}
+
+export interface ISumFields {
+  sumFirstField: number;
+  sumSecondField: number;
+  sumThirdField: number;
+}
